f23e76cfb45633325271abe6142df7a2a4dd2fa4
Suprabhat Maiti <maitisuprabhat@gmail.com>
2025-12-13 20:09:07 +0530

    Add main application structure with routing and initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/layout/Navbar";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./components/layout/Sidebar";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="flex">
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import AddBlogFormPage from "./pages/AddBlogFormPage.jsx";
+import BlogsPage from "./pages/BlogsPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "/blogs",
+        element: <BlogsPage />,
+      },
+      {
+        path: "/add-blog",
+        element: <AddBlogFormPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </StrictMode>
+);
