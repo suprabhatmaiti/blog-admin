c6968b31ba5f1c608a8908a4ada2e88e4cc82075
Suprabhat Maiti <maitisuprabhat@gmail.com>
2025-12-13 22:16:03 +0530

    Enhance Sidebar component with navigation links and icons; improve styling

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -6,14 +6,19 @@ import {
 } from "react-icons/ai";
 import { GrLogout } from "react-icons/gr";
 import { LuLayoutDashboard } from "react-icons/lu";
-import { FiFileText } from "react-icons/fi";
+import { FiFileText, FiShield } from "react-icons/fi";
 import { FcManager } from "react-icons/fc";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const toggleSidebar = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
+  const activeClass =
+    " flex gap-2 items-center text-white font-bold bg-red-500 px-2 py-2 rounded-lg cursor-pointer";
+  const inActiveClass =
+    "flex gap-2 items-center hover:bg-gray-200 px-2 py-2 rounded-lg cursor-pointer";
 
   return (
     <div className="relative">
@@ -25,41 +30,56 @@ export default function Sidebar() {
       </button>
 
       <div
-        className={` h-screen flex flex-col transform-all duration-300 hidden md:flex border-r border-gray-300  `}
+        className={` h-screen flex flex-col transform-all duration-300 hidden md:flex border-r border-gray-300   `}
       >
-        <div className=" flex gap-4 justify-center items-center border-b border-gray-200 px-8 py-4">
-          <h1 className="text-3xl bg-[#ea2a60] text-white px-4 py-1 rounded-lg  font-bold">
-            B
+        <div className=" flex gap-4 justify-center items-center mb-4 px-8 py-4">
+          <h1 className="text-3xl bg-red-500 text-white px-2 py-1 rounded-lg  font-bold">
+            <FiShield />
           </h1>
           <h1 className="font-bold text-2xl text-center text-gray-600">
-            Blog<span className=" text-[#ea2a33] ">Admin</span>
+            Blog<span className=" text-red-500 ">Admin</span>
           </h1>
         </div>
         <div className="flex flex-col justify-between flex-grow  ">
-          <div className="space-y-3 font-semibold text-gray-600 text-lg py-3 px-4">
-            <div className="flex gap-2 items-center hover:bg-gray-200 px-2 py-1 rounded-lg cursor-pointer">
+          <nav className="space-y-3 font-semibold text-gray-600 text-lg py-3 px-4">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive ? activeClass : inActiveClass
+              }
+            >
               <LuLayoutDashboard />
               Dashboard
-            </div>
-            <div className="flex gap-2 items-center hover:bg-gray-200 px-2 py-1 rounded-lg cursor-pointer">
+            </NavLink>
+            <NavLink
+              to={"/blogs"}
+              className={({ isActive }) =>
+                isActive ? activeClass : inActiveClass
+              }
+            >
               <FiFileText />
               All Blogs
-            </div>
-            <div className="flex gap-2 items-center hover:bg-gray-200 px-2 py-1 rounded-lg cursor-pointer">
+            </NavLink>
+            <NavLink
+              to={"/add-blog"}
+              className={({ isActive }) =>
+                isActive ? activeClass : inActiveClass
+              }
+            >
               <AiOutlineFileAdd /> Add New Blog
-            </div>
-          </div>
+            </NavLink>
+          </nav>
           <div className="px-4 py-5 bg-gray-100 w-full flex justify-between items-center ">
-            <div className="flex gap-4 items-center justify-start">
+            <div className="flex gap-4 items-center justify-center">
               <FcManager className="text-4xl border border-gray-400 rounded-full" />
               <div>
                 <h1>Admin</h1>
                 <p>admin@blog.com</p>
               </div>
             </div>
-            <button className="  hover:text-red-600 cursor-pointer text-lg font-semibold flex justify-end">
+            {/* <button className="  hover:text-red-600 cursor-pointer text-lg font-semibold flex justify-end">
               <GrLogout className="text-xl font-bold" />
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
