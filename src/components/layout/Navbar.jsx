8d5f23465d36027d5d1ab9ec01a2efced58c35e5
Suprabhat Maiti <maitisuprabhat@gmail.com>
2025-12-15 17:18:43 +0530

    Refactor Navbar component; remove unused search and add blog functionality for cleaner code

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,45 +4,11 @@ import { IoMdAdd } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
-  const onSearch = (e) => {
-    e.preventDefault();
-    console.log("search");
-  };
-  const navigate = useNavigate();
-
-  const onAddBlog = () => {
-    navigate("/add-blog");
-  };
-
   return (
-    <div className="border-b border-gray-200 px-8 py-4 flex justify-between items-center w-full">
-      <form
-        onSubmit={onSearch}
-        className="w-1/2 flex gap-2 items-center justify-between"
-      >
-        <input
-          type="text"
-          className="border border-gray-200 rounded-full px-4 py-2 w-full"
-          placeholder="Search posts, authors .... "
-        />
-        <button
-          type="submit"
-          className="text-2xl hover:bg-gray-200 rounded-full p-2 cursor-pointer"
-        >
-          <FcSearch />
-        </button>
-      </form>
-      <div className="flex gap-4">
-        {/* <div
-          onClick={onAddBlog}
-          className="rounded-full px-4 py-2 flex justify-center items-center gap-2 cursor-pointer bg-red-500 text-white hover:bg-red-600 font-semibold"
-        >
-          <div className="text-xl font-bold">
-            <IoMdAdd />
-          </div>
-          Add Blog
-        </div> */}
-        <div className="text-2xl rounded-full cursor-not-allowed border border-gray-200 p-2">
+    <div className="border-b border-slate-200 px-8 py-4 flex justify-between items-center w-full bg-white">
+      <div className="flex gap-4 justify-between">
+        <div></div>
+        <div className="text-2xl rounded-full cursor-not-allowed border border-slate-200 p-2 bg-slate-50 text-slate-600">
           <FcManager />
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.jsx";
-import AddBlogFormPage from "./pages/BlogAddForm/AddBlogFormPage.jsx";
+import AddBlogFormPage from "./pages/BlogForm/BlogAddForm/AddBlogFormPage.jsx";
 import BlogsPage from "./pages/BlogsPage/Blogs.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
